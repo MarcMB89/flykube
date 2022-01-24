0898f428659c8982ccc56cbb905cdf85a0fc9d78
Marc Martin <marcmartinbarrios@gmail.com>
2022-01-24 16:45:46 +0100

    Create Flykube component

---
+++ src/components/Flykube.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './flykubeStyles.css';
+
+const FlykubeComponent = () => {
+    return (
+        <>
+        <p>Hello World</p>
+        </>
+    )
+}
+
+export default FlykubeComponent;
