c4e7b3f314db4211bb77f258d955002b1faab088
Marc Martin <marcmartinbarrios@gmail.com>
2022-01-24 17:18:10 +0100

    Add basic html tags to match test requirements

--- src/components/Flykube.jsx
+++ src/components/Flykube.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './flykubeStyles.css';
+import logo from './logo.svg';
 
 const FlykubeComponent = () => {
     return (
-        <>
-        <p>Hello World</p>
-        </>
+        <div>
+            <section className='header__container'>
+                <button className='button__active'>Active</button>
+                <button className='button__inactive'>Inactive</button>
+            </section>
+            <section className='main__container'>
+                <img src={logo} alt="react-logo" />
+            </section>
+            <section className='input__container'>
+            <input></input>
+            <button className='button__send'>Enviar</button>
+            </section>
+        </div>
     )
 }
 
