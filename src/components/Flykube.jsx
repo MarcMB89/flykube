0f97d815892ec42df20739950b2a5f9d6272b93a
Marc Martin <marcmartinbarrios@gmail.com>
2022-01-24 18:39:19 +0100

    Refactor html tags to improve css

--- src/components/Flykube.jsx
+++ src/components/Flykube.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import './flykubeStyles.css';
-import logo from './logo.svg';
+import logo from './logo.png';
 
 const FlykubeComponent = () => {
     return (
-        <div>
+        <div className='app__body'>
             <section className='header__container'>
-                <button className='button__active'>Active</button>
+                <div className='button__container-active'>
+                    <button className='button__active'>Active</button>
+                </div>
+                <div className='button__container-inactive'>
                 <button className='button__inactive'>Inactive</button>
+                </div>
             </section>
+            
             <section className='main__container'>
                 <img src={logo} alt="react-logo" />
+
+                <section className='input__container'>
+                    <input></input>
+                    <button className='button__send'>ENVIAR</button>
+                </section>
             </section>
-            <section className='input__container'>
-            <input></input>
-            <button className='button__send'>Enviar</button>
-            </section>
+            
         </div>
     )
 }
